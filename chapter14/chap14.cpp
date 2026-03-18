2428b212e1382491c6ac1a4e2716c40025401693
yangyang <yangy129@chinatelecom.cn>
2026-03-18 09:23:40 +0800

    test

---
+++ chapter14/chap14.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+class Date {
+    // class 成员默认是是 private, 只能被其它本类的成员访问, 加 public之后可以被其他类访问
+    // 在C++中，以“m_”前缀开头命名私有数据成员是一种常见的约定
+private:
+    int m_day{};
+    int m_month{};
+    int m_year{};
+
+    // 不加访问修饰符，默认是public的
+public:
+    void print() {
+        std::cout << m_day << "/" << m_month << "/" << m_year << std::endl;
+    }
+
+    // 方法重载
+public:
+    void print(std::string str) {
+        std::cout << m_day << "/" << m_month << "/" << m_year << std::endl;
+        std::cout << str << std::endl;
+    }
+};
+
+
+int main() {
+    std::cout << "hello world!" << std::endl;
+    Date dateObj{1, 2, 2025};
+    dateObj.print();
+    return 0;
+}
